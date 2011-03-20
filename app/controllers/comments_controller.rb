dce9705bb58d32638985965d870f1856c972c283
Anthony Porthouse <Admin@port3m5.com>
2011-03-20 23:09:25 +0000

    Added jQuery and removed Prototype.js. Also added AJAX to comment posting and added a way to delete comments, also with AJAX

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,7 +3,19 @@ class CommentsController < ApplicationController
   def create
     @post = Post.find params[:post_id]
     @comment = @post.comments.create params[:comment]
-    redirect_to post_path(@post)
+    respond_to do |format|
+      format.html { redirect_to post_path(@post, :anchor => @comment.id) }
+      format.js
+    end
   end
 
+  def destroy
+    @post = Post.find params[:post_id]
+    @comment = Comment.find params[:id]
+    @comment.destroy
+    respond_to do |format|
+      format.html { redirect_to post_path @post }
+      format.js
+    end
+  end
 end
